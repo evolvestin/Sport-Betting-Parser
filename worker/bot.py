3fc4ca6310d157f963641406942b1c97733e29ac
evolvestin <evolvestin@gmail.com>
2022-04-24 19:00:27 +0300

    Beta 1.0.19

--- worker/bot.py
+++ worker/bot.py
@@ -74,8 +74,9 @@ def post_updater():
                     bot.edit_message_text(chat_id=os.environ['channel_id'],
                                           text=iter_post(record), message_id=record['post_id'],
                                           disable_web_page_preview=True, parse_mode='HTML')
+                    db.update('main', record['id'], {'post_update': time_now()})
                 except IndexError and Exception:
-                    pass
+                    Auth.dev.executive(None)
             sleep(100)
         except IndexError and Exception:
             Auth.dev.thread_except()
