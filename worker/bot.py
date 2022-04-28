3713baf403f6f14d3e3c8e1f7ac42061b79e8151
evolvestin <evolvestin@gmail.com>
2022-04-28 15:26:15 +0300

    Beta 1.0.37

--- worker/bot.py
+++ worker/bot.py
@@ -75,6 +75,116 @@ def iter_post(record):
     return text
 
 
+def handler(driver: chrome, old: bool = False):
+    db = SQL('db/database.db')
+    driver.set_window_size(1200, 1200)
+    driver.get(os.environ.get('link'))
+    body = driver.find_element(By.TAG_NAME, 'tbody')
+
+    if old:
+        before = datetime.now(tz) - timedelta(days=1)
+        for label in driver.find_elements(By.TAG_NAME, 'label'):
+            if label.text == before.strftime('%d-%m'):
+                label.click()
+                break
+        sleep(5)
+
+    for tr in body.find_elements(By.TAG_NAME, 'tr'):
+        coefficient_1, coefficient_2 = None, None
+        odds = tr.find_elements(By.CLASS_NAME, f"{os.environ.get('tag1')}__odd")
+        bet = tr.find_element(By.CLASS_NAME, f"{os.environ.get('tag1')}__bet").text
+        game_id, percent_1, percent_2 = tr.get_attribute('data-eventid'), None, None
+        title = tr.find_element(By.CLASS_NAME, f"{os.environ.get('tag1')}__teams").text
+        percents = tr.find_elements(By.CLASS_NAME, f"{os.environ.get('tag1')}__percent")
+        start_time = tr.find_element(By.CLASS_NAME, f"{os.environ.get('tag1')}__time").text
+
+        if len(percents) == 3:
+            percent_1, percent_2 = percents[0].text or None, percents[2].text or None
+
+        if len(odds) == 3:
+            coefficient_1 = odds[0].text if odds[0].text != '0.00' else None
+            coefficient_2 = odds[2].text if odds[2].text != '0.00' else None
+
+        tds = tr.find_elements(By.TAG_NAME, 'td')
+        for td in tds:
+            score = td.get_attribute('data-res')
+            if score:
+                record = db.get_row(game_id)
+                if record:
+                    if record['score'] != score or \
+                            record['coefficient_1'] != coefficient_1 or \
+                            record['coefficient_2'] != coefficient_2 or \
+                            record['percent_1'] != percent_1 or record['percent_2'] != percent_2:
+                        db.update('main', game_id, {
+                            'name': title,
+                            'score': score,
+                            'percent_1': percent_1,
+                            'percent_2': percent_2,
+                            'coefficient_1': coefficient_1,
+                            'coefficient_2': coefficient_2})
+                else:
+                    if old is False:
+                        now, update = datetime.now(tz), {}
+                        posting = True if score == '- : -' else None
+                        play_time = datetime.fromisoformat(f"{now.strftime('%Y-%m-%d')} {start_time}:00+03:00")
+                        record = {
+                            'bet': bet,
+                            'id': game_id,
+                            'name': title,
+                            'ended': None,
+                            'score': score,
+                            'post_id': None,
+                            'percent_1': percent_1,
+                            'percent_2': percent_2,
+                            'coefficient_1': coefficient_1,
+                            'coefficient_2': coefficient_2,
+                            'start_time': play_time.timestamp(),
+                            'post_update': zero_row['post_update']}
+                        db.create_row(record, google_update=False)
+                        try:
+                            coefficient = coefficient_1 if bet == 'П1' else None
+                            coefficient = coefficient_2 if bet == 'П2' else coefficient
+                            fl_coefficient = float(coefficient) if coefficient else None
+                            posting = posting if fl_coefficient >= 1.4 else None
+                        except IndexError and Exception:
+                            pass
+
+                        if posting:
+                            try:
+                                text = iter_post(record)
+                                post = bot.send_message(os.environ['channel_id'], text,
+                                                        disable_web_page_preview=True, parse_mode='HTML')
+                                update = {'post_id': post.id, 'post_update': time_now()}
+                                sleep(60)
+                            except IndexError and Exception:
+                                Auth.dev.executive(None)
+                        db.update('main', game_id, update)
+    db.close()
+
+
+def parser():
+    while True:
+        try:
+            driver = chrome(os.environ.get('local'))
+            handler(driver)
+            driver.close()
+            sleep(300)
+        except IndexError and Exception:
+            Auth.dev.thread_except()
+
+
+def old_parser():
+    while True:
+        try:
+            sleep(300)
+            driver = chrome(os.environ.get('local'))
+            handler(driver, old=True)
+            driver.close()
+            sleep(300)
+        except IndexError and Exception:
+            Auth.dev.thread_except()
+
+
 def post_ender():
     while True:
         try:
@@ -85,6 +195,25 @@ def post_ender():
                 db.update('main', record['id'], {'ended': '🔒', 'post_update': 946674000})
             db.close()
             sleep(60)
+        except IndexError and Exception:
+            Auth.dev.thread_except()
+
+
+def auto_reboot():
+    reboot = None
+    while True:
+        try:
+            sleep(30)
+            date = datetime.now(tz)
+            if date.strftime('%H') == '01' and date.strftime('%M') == '59':
+                reboot = True
+                while date.strftime('%M') == '59':
+                    sleep(1)
+                    date = datetime.now(tz)
+            if reboot:
+                reboot = None
+                text, _ = Auth.logs.reboot()
+                Auth.dev.printer(text)
         except IndexError and Exception:
             Auth.dev.thread_except()
 
@@ -113,25 +242,6 @@ def post_updater():
                 sleep(25)
             db.close()
             sleep(5)
-        except IndexError and Exception:
-            Auth.dev.thread_except()
-
-
-def auto_reboot():
-    reboot = None
-    while True:
-        try:
-            sleep(30)
-            date = datetime.now(tz)
-            if date.strftime('%H') == '01' and date.strftime('%M') == '59':
-                reboot = True
-                while date.strftime('%M') == '59':
-                    sleep(1)
-                    date = datetime.now(tz)
-            if reboot:
-                reboot = None
-                text, _ = Auth.logs.reboot()
-                Auth.dev.printer(text)
         except IndexError and Exception:
             Auth.dev.thread_except()
 
@@ -180,90 +290,6 @@ def google_update():
             Auth.dev.thread_except()
 
 
-def parser():
-    while True:
-        try:
-            db = SQL('db/database.db')
-            driver = chrome(os.environ.get('local'))
-            driver.set_window_size(1200, 1200)
-            driver.get(os.environ.get('link'))
-            body = driver.find_element(By.TAG_NAME, 'tbody')
-            for tr in body.find_elements(By.TAG_NAME, 'tr'):
-                coefficient_1, coefficient_2 = None, None
-                odds = tr.find_elements(By.CLASS_NAME, f"{os.environ.get('tag1')}__odd")
-                bet = tr.find_element(By.CLASS_NAME, f"{os.environ.get('tag1')}__bet").text
-                game_id, percent_1, percent_2 = tr.get_attribute('data-eventid'), None, None
-                title = tr.find_element(By.CLASS_NAME, f"{os.environ.get('tag1')}__teams").text
-                percents = tr.find_elements(By.CLASS_NAME, f"{os.environ.get('tag1')}__percent")
-                start_time = tr.find_element(By.CLASS_NAME, f"{os.environ.get('tag1')}__time").text
-
-                if len(percents) == 3:
-                    percent_1, percent_2 = percents[0].text or None, percents[2].text or None
-
-                if len(odds) == 3:
-                    coefficient_1 = odds[0].text if odds[0].text != '0.00' else None
-                    coefficient_2 = odds[2].text if odds[2].text != '0.00' else None
-
-                tds = tr.find_elements(By.TAG_NAME, 'td')
-                for td in tds:
-                    score = td.get_attribute('data-res')
-                    if score:
-                        record = db.get_row(game_id)
-                        if record:
-                            if record['score'] != score or \
-                                    record['coefficient_1'] != coefficient_1 or \
-                                    record['coefficient_2'] != coefficient_2 or \
-                                    record['percent_1'] != percent_1 or record['percent_2'] != percent_2:
-                                db.update('main', game_id, {
-                                    'name': title,
-                                    'score': score,
-                                    'percent_1': percent_1,
-                                    'percent_2': percent_2,
-                                    'coefficient_1': coefficient_1,
-                                    'coefficient_2': coefficient_2})
-                        else:
-                            now, update = datetime.now(tz), {}
-                            posting = True if score == '- : -' else None
-                            play_time = datetime.fromisoformat(f"{now.strftime('%Y-%m-%d')} {start_time}:00+03:00")
-                            record = {
-                                'bet': bet,
-                                'id': game_id,
-                                'name': title,
-                                'ended': None,
-                                'score': score,
-                                'post_id': None,
-                                'percent_1': percent_1,
-                                'percent_2': percent_2,
-                                'coefficient_1': coefficient_1,
-                                'coefficient_2': coefficient_2,
-                                'start_time': play_time.timestamp(),
-                                'post_update': zero_row['post_update']}
-                            db.create_row(record, google_update=False)
-                            try:
-                                coefficient = coefficient_1 if bet == 'П1' else None
-                                coefficient = coefficient_2 if bet == 'П2' else coefficient
-                                fl_coefficient = float(coefficient) if coefficient else None
-                                posting = posting if fl_coefficient >= 1.4 else None
-                            except IndexError and Exception:
-                                pass
-
-                            if posting:
-                                try:
-                                    text = iter_post(record)
-                                    post = bot.send_message(os.environ['channel_id'], text,
-                                                            disable_web_page_preview=True, parse_mode='HTML')
-                                    update = {'post_id': post.id, 'post_update': time_now()}
-                                    sleep(60)
-                                except IndexError and Exception:
-                                    Auth.dev.executive(None)
-                            db.update('main', game_id, update)
-            driver.close()
-            db.close()
-            sleep(300)
-        except IndexError and Exception:
-            Auth.dev.thread_except()
-
-
 def start(stamp):
     try:
         if os.environ.get('local'):
@@ -271,7 +297,7 @@ def start(stamp):
             Auth.dev.printer(f'Запуск бота локально за {time_now() - stamp} сек.')
         else:
             Auth.dev.start(stamp)
-            threads = [parser, google_update, post_updater, post_ender]
+            threads = [parser, old_parser, google_update, post_updater, post_ender]
             Auth.dev.printer(f'Бот запущен за {time_now() - stamp} сек.')
 
         for thread_element in threads:
